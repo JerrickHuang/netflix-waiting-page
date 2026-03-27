3e7bb8eb2526f41b5ccbf2e890f1fbb596a2768a
jerrick <twjhuang@gmail.com>
2026-03-27 19:06:33 +0800

    Add rotating comforting messages to reduce user anxiety

--- script.js
+++ script.js
@@ -33,3 +33,31 @@ width + "%";
 }
 
 }, 600);
+
+let messages = [
+
+"工程師正在追進度中 🚀",
+
+"你的爆米花還沒涼 🍿",
+
+"下一集沒有消失放心 🎬",
+
+"伺服器正在全力修復中"
+
+];
+
+let messageText =
+
+document.getElementById("message");
+
+setInterval(() => {
+
+let randomIndex =
+
+Math.floor(Math.random()*messages.length);
+
+messageText.innerText =
+
+messages[randomIndex];
+
+},5000);
