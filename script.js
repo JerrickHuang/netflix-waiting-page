4413a958ba5fac34ae6c8cf3aca4a9a3b14452d4
jerrick <twjhuang@gmail.com>
2026-03-27 19:05:25 +0800

    Add animated loading progress bar effect

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ countdownElement.innerText =
 "預估恢復時間：" + time + " 秒";
 
 }, 1000);
+
+let progress =
+
+document.querySelector(".progress");
+
+let width = 0;
+
+setInterval(() => {
+
+if(width < 100){
+
+width++;
+
+progress.style.width =
+
+width + "%";
+
+}
+
+}, 600);
